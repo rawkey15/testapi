a43da241f2ee83d91908839bb8a5a2cb8f2feae0
LAPTOP-JTQC4GRL\Rawkey <rawkey05@gmail.com>
2018-08-09 21:49:45 +0530

    no message

--- index.js
+++ index.js
@@ -1,87 +1,10 @@
-var express = require('express');
-var app = express();
-var cricapi = require("node-cricapi");
-var request = require('request');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.set('port', (process.env.PORT || 5000));
-
-//setup cross-origin
-
-var cors =  {
-
-            origin: ["https://rakesh-angular-dashboard.herokuapp.com"],
-
-            default: "https://rakesh-angular-dashboard.herokuapp.com" 
-        }
-
-app.use(function(req, res, next) {
-
-  var origins = cors.origin.indexOf(req.header('origin')) > -1 ? req.headers.origin : cors.default;
-
-  res.header("Access-Control-Allow-Origin", origins);
-
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-  next();
-
-});
-
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-//Router for rest api
-
-var v3_apikey = '32047621915820fa1caf5db2a806146e';
-var v4_authToken = 'BearereyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjA0NzYyMTkxNTgyMGZhMWNhZjVkYjJhODA2MTQ2ZSIsInN1YiI6IjU5NDAyMzc2OTI1MTQxN2E4MzAwOGEwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3sGZZ6y-MFJPf7GVjP8sgrxqw_O0iXzXWXMn_iD0c2k';
-
-app.get('/search/:movie', function(req, res, next) {
-var moviename = req.params.movie;
-  request({
-    uri: 'https://api.themoviedb.org/3/search/movie?api_key='+v3_apikey+'&query='+moviename
-  }).pipe(res);
-});
-
-app.get('/movie/:id', function(req, res, next) {
-var movieid = req.params.id;
-  request({
-    uri: 'https://api.themoviedb.org/3/movie/'+movieid+'?api_key='+v3_apikey+'&append_to_response=videos'
-  }).pipe(res);
-});
-
-app.get('/movieslist', function(req, res, next) {
-
-var options = { method: 'GET',
-  url: 'https://api.themoviedb.org/4/list/1',
-  qs: 
-   { language: 'english',
-     api_key: v3_apikey,
-     page: '1' },
-  headers: 
-   { authorization: v4_authToken,
-     'content-type': 'application/json;charset=utf-8' },
-  body: {},
-  json: true };
-
-request(options).pipe(res);
-});
-
-
-
-
-
-
-
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
